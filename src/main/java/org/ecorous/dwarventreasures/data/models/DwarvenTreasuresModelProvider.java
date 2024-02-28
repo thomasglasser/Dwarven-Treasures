593115e2ef2376036c4c6daa54a5fdde23721bcd
Thomas Glasser <tjglass06@gmail.com>
2024-02-27 19:14:06 -0600

    Updated deps Added model gen for 4 layered blocks

--- src/main/java/org/ecorous/dwarventreasures/data/models/DwarvenTreasuresModelProvider.java
+++ src/main/java/org/ecorous/dwarventreasures/data/models/DwarvenTreasuresModelProvider.java
@@ -4,8 +4,18 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.models.BlockModelGenerators;
 import net.minecraft.data.models.ItemModelGenerators;
+import net.minecraft.data.models.blockstates.MultiVariantGenerator;
+import net.minecraft.data.models.blockstates.PropertyDispatch;
+import net.minecraft.data.models.blockstates.Variant;
+import net.minecraft.data.models.blockstates.VariantProperties;
+import net.minecraft.data.models.blockstates.VariantProperty;
+import net.minecraft.data.models.model.ModelLocationUtils;
 import net.minecraft.data.models.model.ModelTemplates;
+import net.minecraft.data.models.model.TextureMapping;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.ecorous.dwarventreasures.world.item.DwarvenTreasuresItems;
 import org.ecorous.dwarventreasures.world.level.block.DwarvenTreasuresBlocks;
 
@@ -47,5 +57,26 @@ public class DwarvenTreasuresModelProvider extends FabricModelProvider {
     public static void handheld(ItemModelGenerators itemModelGenerators, Item item)
     {
         itemModelGenerators.generateFlatItem(item, ModelTemplates.FLAT_HANDHELD_ITEM);
+    }
+
+    protected void createFourLayeredBlocks(BlockModelGenerators blockModelGenerators, Block block, Block fullBlock) {
+        TextureMapping textureMapping = TextureMapping.cube(block);
+        ResourceLocation resourceLocation = ModelTemplates.CUBE_ALL.create(fullBlock, textureMapping, blockModelGenerators.modelOutput);
+        blockModelGenerators.blockStateOutput.accept(MultiVariantGenerator.multiVariant(block).with(PropertyDispatch.property(BlockStateProperties.LAYERS).generate((integer) -> {
+            Variant var10000 = Variant.variant();
+            VariantProperty var10001 = VariantProperties.MODEL;
+            ResourceLocation var2;
+            if (integer < 8) {
+                Block var10002 = block;
+                int var10003 = integer;
+                var2 = ModelLocationUtils.getModelLocation(var10002, "_height" + var10003 * 2);
+            } else {
+                var2 = resourceLocation;
+            }
+
+            return var10000.with(var10001, var2);
+        })));
+        blockModelGenerators.delegateItemModel(block, ModelLocationUtils.getModelLocation(block, "_height2"));
+        blockModelGenerators.blockStateOutput.accept(BlockModelGenerators.createSimpleBlock(fullBlock, resourceLocation));
     }
 }
